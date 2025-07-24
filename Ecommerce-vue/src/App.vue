eb0402007fed13f35cbea8d73d0c827f42d1540b
Godwin-AKAKPO <mouyiwaakakpo@gmail.com>
2025-07-24 17:08:20 +0100

    First step of product

--- Ecommerce-vue/src/App.vue
+++ Ecommerce-vue/src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <Header/>
   <Hero/>
+  <Products/>
 </template>
 
 <script setup>
   import Header from './components/Header.vue';
-  import Hero from './components/Hero.vue'
+  import Hero from './components/Hero.vue';
+  import Products from './components/Products.vue'
 </script>
 
 <style>
