f6853507f76e44f182c9ab36eeea42a602c1b804
Godwin-AKAKPO <mouyiwaakakpo@gmail.com>
2025-07-24 12:47:38 +0100

    Here we go

--- Ecommerce-vue/src/App.vue
+++ Ecommerce-vue/src/App.vue
@@ -1,9 +1,11 @@
 <template>
   <Header/>
+  <Hero/>
 </template>
 
 <script setup>
   import Header from './components/Header.vue';
+  import Hero from './components/Hero.vue'
 </script>
 
 <style>
---
+++ Ecommerce-vue/src/components/Hero.vue
@@ -0,0 +1,31 @@
+<template>
+    <section id="home" class="bg-amber-50 scroll-mt-20 py-16">
+        <div class="max-w-7x1 mx-auto px-4 sm:px-6 1g:px-8 flex-col-reverse lg:flex-row items-center gap-10">
+            <!-- Left Content -->
+            <div class="text-center 1g:text-left max-w-x-&">
+                <h1 class="text-4xl sm:text-5xl font-bold text-amber-950 leading-tight mb-4">
+                    Discover the Best Deals on Top Products 
+                </h1>
+                <p class="text-gray-700 text-base sm:text-1g mb-6">
+                    Exclusive discounts and special offers on everything you love. Shop now and be th eifrst to 
+                    grab the latest trends
+                </p>
+                <div class="flex justity-center lg:justify-start gap-4">
+                    <a href="/products"
+                    class="px-6 py-3 bg-pink-900 text-white text-sm font-semibold rounded-lg shadow 
+                    hover:bg-pink-600 transition"> Shop Now</a>
+                    <a href="/products"
+                    class="px-6 py-3 bg-pink-500 text-pink-250 text-sm font-semibold rounded-lg shadow 
+                    hover:bg-pink-200 transition">View Offers</a>
+                </div>
+            </div>
+            <!-- Right Image  -->
+            <div class="w-full lg:w-1/2 flex justify-center">
+                <img src="" alt="" class="w-3/4 lg:">
+            </div>
+        </div>
+
+    </section>
+</template>
+<script setup> 
+</script>
